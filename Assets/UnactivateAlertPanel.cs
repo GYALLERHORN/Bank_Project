19bdd7a43af0a9f9ca8324f9efb00fb1db273c9a
GYALLERHORN <kjins1998@gmail.com>
2023-09-22 21:45:30 +0900

    UI 텍스트 수정

--- Assets/UnactivateAlertPanel.cs
+++ Assets/UnactivateAlertPanel.cs
@@ -8,12 +8,12 @@ public class UnactivatePanel : MonoBehaviour
     [SerializeField] private GameObject alertBoard;
     [SerializeField] private TMP_Text alertDetail;
 
-    private void Start()
+    private void OnEnable()
     {
-        alertDetail.text = $"Exchange is more then Cash";
+        alertDetail.text = "Exchange is bigger than Cash";
         if (!GameManager.instance.isDepositRunning)
         {
-            alertDetail.text = $"Exchange is more then Account"; 
+            alertDetail.text = "Exchange is bigger than Account"; 
         }
     }
 
